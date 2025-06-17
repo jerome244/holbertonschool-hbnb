9c9d63a4f3c4170dfd52090b637ad00fc67fa449
jerome244 <10485@gmail.com>
2025-06-17 15:50:05 +0200

    fix: rm old stuffs

--- part2/business/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from .user import User
-from .host import Host
-from .place import Place
-from .booking import Booking
-from .review import Review
-from .amenity import Amenity  # only if you have this file
--- part2/hbnb/app/api/__init__.py
+++
@@ -1,7 +0,0 @@
-# app/api/__init__.py
-from flask import Blueprint
-
-api_bp = Blueprint('api', __name__)
-
-from app.api.v1.users import users_bp
-api_bp.register_blueprint(users_bp, url_prefix='/users')
