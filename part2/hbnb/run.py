1d39e2ab2d1529e6ea2d078d6b1c84b95fd4c010
jerome244 <10485@holbertonstudents.com>
2025-06-19 21:20:55 +0200

    fix: rm lines

--- part2/hbnb/run.py
+++ part2/hbnb/run.py
@@ -2,10 +2,5 @@ from app import create_app
 
 app = create_app()
 
-<<<<<<< HEAD
-if __name__ == '__main__':
-    app.run(debug=True)
-=======
 if __name__ == "__main__":
     app.run(debug=True)
->>>>>>> devJerome
