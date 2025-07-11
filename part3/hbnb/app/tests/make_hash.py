061cd6cf6c4f3465367ffaaabeedb55f82137b9b
Theo-D <theodessaigne@hotmail.fr>
2025-07-11 10:41:14 +0200

    Added make_hash.py a script to generate and test hashed password

---
+++ part3/hbnb/app/tests/make_hash.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+"""
+    A function to generate an hash compatible
+    with werkzeug password check function.
+"""
+
+from werkzeug.security import generate_password_hash, check_password_hash
+
+def make_hash(password, test_password):
+
+    hashed = generate_password_hash(password)
+    print(f"Original password: {password}")
+    print(f"Hashed password: {hashed}")
+
+    is_hashed = check_password_hash(hashed, test_password)
+
+    if is_hashed:
+        print("test_password and hashed are matching")
+    else:
+        print("test_password and hashed are not matching")
+
+    return is_hashed
+
+if __name__ == "__main__":
+
+    original_password = "admin1234"
+    input_password = "admin1234"
+
+    make_hash(original_password, input_password)
